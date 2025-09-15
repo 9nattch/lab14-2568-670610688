c0c25de03330ae04ac937e93303942c1bbcf4e0f
9nattch <nattchayadao@gmail.com>
2025-09-15 20:39:57 +0700

    done

--- src/store/MarathonFormStore.tsx
+++ src/store/MarathonFormStore.tsx
@@ -5,7 +5,12 @@ export const useMarathonFormStore = create<MarathonFormState>((set) => ({
   lname: "",
   plan: "funrun",
   gender: "male",
+  total: 0,
   email: "",
+  password: "",
+  confirmpassword: "",
+  haveCoupon: false,
+  couponCode: "",
   setFname: (fname) =>
     set(() => ({
       fname: fname,
@@ -26,7 +31,40 @@ export const useMarathonFormStore = create<MarathonFormState>((set) => ({
     set(() => ({
       email: _email,
     })),
+  setPassword: (_password) =>
+    set(() => ({
+      password: _password,
+    })),
+  setConfirmpassword: (_confirmpassword) =>
+    set(() => ({
+      confirmpassword: _confirmpassword,
+    })),
+  sethaveCoupon: (_haveCoupon) =>
+    set(() => ({
+      haveCoupon: _haveCoupon,
+    })),
+  setcouponCode: (_couponCode) =>
+    set(() => ({
+      couponCode: _couponCode,
+    })),
+
   // Function ชื่อ discountCupon คำนวณ total ตรงนี้
+
+  discountCupon: () =>
+    set((state) => {
+      let totalPayment = 0;
+      if (state.plan === "funrun") totalPayment += 500;
+      if (state.plan === "mini") totalPayment += 800;
+      if (state.plan === "half") totalPayment += 1200;
+      if (state.plan === "full") totalPayment += 1500;
+
+      if (state.haveCoupon && state.couponCode?.trim() === "CMU2025") {
+        const discount = totalPayment * 0.3;
+        totalPayment -= discount;
+      }
+
+      return { total: totalPayment };
+    }),
   reset: () =>
     set({
       fname: "",
@@ -34,5 +72,10 @@ export const useMarathonFormStore = create<MarathonFormState>((set) => ({
       plan: "funrun",
       gender: "male",
       email: "",
+      password: "",
+      confirmpassword: "",
+      haveCoupon: false,
+      couponCode: "",
+      total: 0,
     }),
 }));
--- src/zod/MarathonSchema.tsx
+++ src/zod/MarathonSchema.tsx
@@ -6,13 +6,25 @@ export const marathonSchema = z
     fname: z
       .string()
       .min(3, { message: "First name must have at least 3 letters" }),
-    lname: z.string().min(5, "Last name must have at least 5 letters"),
-    plan: z.enum(["funrun", "mini", "half", "full"], {
-      message: "Select a plan",
-    }),
-    gender: z.enum(["male", "female"], { message: "Select gender" }),
-    agree: z.boolean().default(false),
-    email: z.email(),
+    lname: z
+      .string()
+      .min(5, "Last name must have at least 5 letters"),
+    plan: z
+      .enum(["funrun", "mini", "half", "full"], {message: "Select a plan", }),
+    gender: z
+      .enum(["male", "female"], { message: "Select gender" }),
+    agree: z
+      .boolean()
+      .default(false),
+    email: z
+    .email(),
+    password: z
+      .string()
+      .min(6,{message:"Password must contain at least 6 characters"})
+      .max(12,{message:"Password must not exceed 12 characters"}),
+    confirmpassword: z
+    .string()
+    .min(1,{message:"Please confirm password"}),
     haveCoupon: z.boolean().default(false),
     couponCode: z.string().optional(),
   })
@@ -24,6 +36,14 @@ export const marathonSchema = z
     {
       message: "Invalid coupon code",
       path: ["couponCode"],
+    }
+    
+  )
+  .refine(
+    (data) => data.password === data.confirmpassword,
+    {
+      message:"Password does not match",
+      path: ["confirmpassword"],
     }
   );
 export type MarathonForm = z.infer<typeof marathonSchema>;
